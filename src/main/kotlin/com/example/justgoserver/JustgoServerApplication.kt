9aa587095316bd7254f3a1cabed7141f30a9100a
4mjeo <4mj.eo@dsm.hs.kr>
2024-07-16 11:15:24 +0900

    Initial commit

---
+++ src/main/kotlin/com/example/justgoserver/JustgoServerApplication.kt
@@ -0,0 +1,11 @@
+package com.example.justgoserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JustgoServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<JustgoServerApplication>(*args)
+}
